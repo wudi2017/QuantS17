0d303d0d8d403ea2cd270a171b0c91772957e4a7
wudi <wudi19870903@163.com>
2017-12-09 16:07:56 +0800

    x

--- QuantS1711/src/strategy/QS1711.java
+++ QuantS1711/src/strategy/QS1711.java
@@ -30,219 +30,132 @@ import utils.PricePosChecker.ResultDropParam;
  * 选股
  */
 public class QS1711 {
-	public static class QS1711Strategy extends QuantStrategy
+	public static class QS1711Strategy extends QS1711Base
 	{
 		public QS1711Strategy()
 		{
+			super(10, 5); // maxSelect=10 maxHold=5
 		}
-		
-		@Override
-		public void onInit(QuantContext ctx) {
-			m_XStockSelectManager = new XStockSelectManager(ctx.ap());
-			m_TranReportor = new TranReportor("R");
-		}
-		@Override
-		public void onDayStart(QuantContext ctx) {
-			CLog.output("TEST", "onDayStart %s", ctx.date());
-			m_XStockSelectManager.loadFromFile();
-			super.addCurrentDayInterestMinuteDataIDs(m_XStockSelectManager.validSelectListS1(30));
-			CLog.output("TEST", "%s", m_XStockSelectManager.dumpSelect());
-		}
-		
-		public void onBuyCheck(QuantContext ctx)
+
+		void onStrateInit(QuantContext ctx)
 		{
-			List<String> validSelectList = m_XStockSelectManager.validSelectListS1(10);
-			for(int iStock=0; iStock<validSelectList.size(); iStock++)
-			{
-				String stockID = validSelectList.get(iStock);
-				
-				DAStock cDAStock = ctx.pool().get(stockID);
-				double fYesterdayClosePrice = cDAStock.dayKLines().lastPrice();
-				double fNowPrice = cDAStock.price();
-				
-				boolean bBuyFlag = false;
-				do
-				{
-					// 1-跌停不买进
-					double fYC = CUtilsMath.saveNDecimal(fYesterdayClosePrice, 2);
-					double fDieTing = CUtilsMath.saveNDecimal(fYC*0.9f, 2);
-					if(0 == Double.compare(fDieTing, fNowPrice))
-					{
-						bBuyFlag = false;
-						break;
-					}
-					
-					// 2-近期涨幅过大不买进（根据选股条件计算买入参数）
-					boolean bCheckFlg = false;
-					int iZCZXFindEnd = -1;
-					DAKLines list = cDAStock.dayKLines();
-					int iCheck = list.size()-2;
-					
-					int iBegin = iCheck-5;
-					int iEnd = iCheck;
-					
-					for(int i=iEnd;i>=iBegin;i--)
-					{
-						if(ZCZXChecker.check(list,i))
-						{
-							bCheckFlg = true;
-							iZCZXFindEnd = i;
-							break;
-						}
-					}
-					
-					if(bCheckFlg && -1!=iZCZXFindEnd)
-					{
-						KLine cKLineZCZXEnd = cDAStock.dayKLines().get(iZCZXFindEnd);
-						double fStdPaZCZX = (cKLineZCZXEnd.entityHigh() + cKLineZCZXEnd.entityLow())/2;
-						double fZhang = (fNowPrice-fStdPaZCZX)/fStdPaZCZX;
-						if(fZhang > 0.08)
-						{
-							bBuyFlag = false;
-							break;
-						}
-					}
-					else
-					{
-						break;
-					}
-					
-					
-					// 买入标记
-					bBuyFlag = true;
-					
-				} while(false);
-				
-				if(bBuyFlag)
-				{
-					List<HoldStock> ctnHoldStockList = new ArrayList<HoldStock>();
-					ctx.ap().getHoldStockList(ctnHoldStockList);
-					if(ctnHoldStockList.size() < 5)
-					{
-						CObjectContainer<Double> ctnTotalAssets = new CObjectContainer<Double>();
-						ctx.ap().getTotalAssets(ctnTotalAssets);
-						CObjectContainer<Double> ctnMoney = new CObjectContainer<Double>();
-						ctx.ap().getMoney(ctnMoney);
-						double dCreateMoney = (ctnMoney.get() > ctnTotalAssets.get()/5)?ctnTotalAssets.get()/5:ctnMoney.get();
-						int iCreateAmount = (int) (dCreateMoney/fNowPrice)/100*100;
-						if(iCreateAmount > 0)
-						{
-							ctx.ap().pushBuyOrder(stockID, iCreateAmount, fNowPrice);
-						}
-					}
-				}
-			}
 		}
 		
-		public void onSellCheck(QuantContext ctx)
+		void onStrateDayStart(QuantContext ctx)
 		{
-			List<HoldStock> ctnHoldStockList = new ArrayList<HoldStock>();
-			ctx.ap().getHoldStockList(ctnHoldStockList);
-			for(int i=0; i<ctnHoldStockList.size(); i++)
-			{
-				HoldStock cHoldStock = ctnHoldStockList.get(i);
-
-				DAStock cDAStock = ctx.pool().get(cHoldStock.stockID);
-				double fYesterdayClosePrice = cDAStock.dayKLines().lastPrice();
-				double fNowPrice = cDAStock.price();
-				
-				boolean bSellFlag = false;
-				do
-				{
-					if(cHoldStock.availableAmount <= 0)
-					{
-						bSellFlag = false;
-						break;
-					}
-					
-					// 涨停不卖出
-					double fYC = CUtilsMath.saveNDecimal(fYesterdayClosePrice, 2);
-					double fZhangTing = CUtilsMath.saveNDecimal(fYC*1.1f, 2);
-					if(0 == Double.compare(fZhangTing, fNowPrice))
-					{
-						bSellFlag = false;
-						break;
-					}
-					
-					// 持股超时卖出
-					long lHoldDays = TranDaysChecker.check(ctx.pool().get("999999").dayKLines(), cHoldStock.createDate, ctx.date());
-					if(lHoldDays >= 30) 
-					{
-						bSellFlag = true;
-						break;
-					}
-					
-					// 止盈止损卖出
-					if(cHoldStock.refProfitRatio() > 0.1 || cHoldStock.refProfitRatio() < -0.12) 
-					{
-						bSellFlag = true;
-						break;
-					}
-				} while(false);
-					
-				if(bSellFlag)
-				{
-					ctx.ap().pushSellOrder(cHoldStock.stockID, cHoldStock.availableAmount, fNowPrice);
-				}	
-			}
 		}
 		
 		@Override
-		public void onMinuteData(QuantContext ctx) {
-			onBuyCheck(ctx);
-			onSellCheck(ctx);
-		}
-		
-		@Override
-		public void onDayFinish(QuantContext ctx) {
-
-			m_XStockSelectManager.clearSelect();
-
-			for(int iStock=0; iStock<ctx.pool().size(); iStock++)
-			{
-				DAStock cDAStock = ctx.pool().get(iStock);
-				
-				// 过滤：股票ID集合，当天检查
-				boolean bCheckX = false;
-				if(
-					//cDAStock.ID().compareTo("000001") >= 0 && cDAStock.ID().compareTo("000200") <= 0 
-					cDAStock.dayKLines().size()>60
-					&& cDAStock.dayKLines().lastDate().equals(ctx.date())
-					&& cDAStock.circulatedMarketValue() < 1000.0) {	
-					bCheckX = true;
-				}
-				
-				if(bCheckX)
-				{
-					// 5天内存在早晨之星
-					int iBegin = cDAStock.dayKLines().size()-1-5;
-					int iEnd = cDAStock.dayKLines().size()-1;
-					for(int i=iEnd;i>=iBegin;i--)
-					{
-						if(ZCZXChecker.check(cDAStock.dayKLines(),i))
-						{
-							boolean bcheckVolume = ZCZXChecker.check_volume(cDAStock.dayKLines(),i);
-							if(bcheckVolume)
-							{
-								ResultDropParam cResultLongDropParam = PricePosChecker.getLongDropParam(cDAStock.dayKLines(), cDAStock.dayKLines().size()-1);
-								m_XStockSelectManager.addSelect(cDAStock.ID(), -cResultLongDropParam.refHigh);
-							}
-						}
-					}
-				}
-			}
-			m_XStockSelectManager.saveToFile();
+		void onStrateBuyCheck(QuantContext ctx, DAStock cDAStock) {
+			double fYesterdayClosePrice = cDAStock.dayKLines().lastPrice();
+			double fNowPrice = cDAStock.price();
 			
-			// report
-			CObjectContainer<Double> ctnTotalAssets = new CObjectContainer<Double>();
-			ctx.ap().getTotalAssets(ctnTotalAssets);
-			double dSH = ctx.pool().get("999999").price();
-			m_TranReportor.InfoCollector(ctx.date(), ctnTotalAssets.get(), dSH);
-			CLog.output("TEST", "dump account&select\n %s\n    -%s", ctx.ap().dump(), m_XStockSelectManager.dumpSelect());
+			// 1-跌停不买进
+			double fYC = CUtilsMath.saveNDecimal(fYesterdayClosePrice, 2);
+			double fDieTing = CUtilsMath.saveNDecimal(fYC*0.9f, 2);
+			if(0 == Double.compare(fDieTing, fNowPrice))
+			{
+				return;
+			}
+				
+			// 2-近期涨幅过大不买进（根据选股条件计算买入参数）
+			boolean bCheckFlg = false;
+			int iZCZXFindEnd = -1;
+			DAKLines list = cDAStock.dayKLines();
+			int iCheck = list.size()-2;
+			
+			int iBegin = iCheck-5;
+			int iEnd = iCheck;
+			
+			for(int i=iEnd;i>=iBegin;i--)
+			{
+				if(ZCZXChecker.check(list,i))
+				{
+					bCheckFlg = true;
+					iZCZXFindEnd = i;
+					break;
+				}
+			}
+			
+			if(bCheckFlg && -1!=iZCZXFindEnd)
+			{
+				KLine cKLineZCZXEnd = cDAStock.dayKLines().get(iZCZXFindEnd);
+				double fStdPaZCZX = (cKLineZCZXEnd.entityHigh() + cKLineZCZXEnd.entityLow())/2;
+				double fZhang = (fNowPrice-fStdPaZCZX)/fStdPaZCZX;
+				if(fZhang > 0.08)
+				{
+					return;
+				}
+			}
+			else
+			{
+				return;
+			}
+
+			super.tryBuy(ctx, cDAStock.ID());	
 		}
-		
-		private XStockSelectManager m_XStockSelectManager;
-		private TranReportor m_TranReportor;
+
+		@Override
+		void onStrateSellCheck(QuantContext ctx, DAStock cDAStock, HoldStock cHoldStock) {
+			double fYesterdayClosePrice = cDAStock.dayKLines().lastPrice();
+			double fNowPrice = cDAStock.price();
+			
+			if(cHoldStock.availableAmount <= 0)
+			{
+				return;
+			}
+				
+			// 涨停不卖出
+			double fYC = CUtilsMath.saveNDecimal(fYesterdayClosePrice, 2);
+			double fZhangTing = CUtilsMath.saveNDecimal(fYC*1.1f, 2);
+			if(0 == Double.compare(fZhangTing, fNowPrice))
+			{
+				return;
+			}
+				
+			// 持股超时卖出
+			long lHoldDays = TranDaysChecker.check(ctx.pool().get("999999").dayKLines(), cHoldStock.createDate, ctx.date());
+			if(lHoldDays >= 30) 
+			{
+				super.trySell(ctx, cHoldStock.stockID);
+				return;
+			}
+				
+			// 止盈止损卖出
+			if(cHoldStock.refProfitRatio() > 0.1 || cHoldStock.refProfitRatio() < -0.12) 
+			{
+				super.trySell(ctx, cHoldStock.stockID);
+				return;
+			}
+		}
+
+		@Override
+		void onStrateDayFinish(QuantContext ctx, DAStock cDAStock) {
+			// 过滤：股票ID集合，当天检查
+			if(
+				//cDAStock.ID().compareTo("000001") >= 0 && cDAStock.ID().compareTo("000200") <= 0 
+				cDAStock.dayKLines().size()<60
+				|| !cDAStock.dayKLines().lastDate().equals(ctx.date())
+				|| cDAStock.circulatedMarketValue() > 1000.0) {	
+				return;
+			}
+				
+			// 5天内存在早晨之星
+			int iBegin = cDAStock.dayKLines().size()-1-5;
+			int iEnd = cDAStock.dayKLines().size()-1;
+			for(int i=iEnd;i>=iBegin;i--)
+			{
+				if(ZCZXChecker.check(cDAStock.dayKLines(),i))
+				{
+					boolean bcheckVolume = ZCZXChecker.check_volume(cDAStock.dayKLines(),i);
+					if(bcheckVolume)
+					{
+						ResultDropParam cResultLongDropParam = PricePosChecker.getLongDropParam(cDAStock.dayKLines(), cDAStock.dayKLines().size()-1);
+						super.trySelect(ctx, cDAStock.ID(), -cResultLongDropParam.refHigh);
+					}
+				}
+			}
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -253,6 +166,7 @@ public class QS1711 {
 		// create testaccount
 		AccoutDriver cAccoutDriver = new AccoutDriver(CSystem.getRWRoot() + "\\account");
 		cAccoutDriver.load("fast_mock001" ,  new MockAccountOpe(), true);
+		cAccoutDriver.reset(100000);
 		Account acc = cAccoutDriver.account();
 		
 		QuantSession qSession = new QuantSession(
---
+++ QuantS1711/src/strategy/QS1711Base.java
@@ -0,0 +1,166 @@
+package strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pers.di.account.common.HoldStock;
+import pers.di.common.CLog;
+import pers.di.common.CObjectContainer;
+import pers.di.common.CUtilsMath;
+import pers.di.dataapi.common.KLine;
+import pers.di.dataengine.DAKLines;
+import pers.di.dataengine.DAStock;
+import pers.di.quantplatform.QuantContext;
+import pers.di.quantplatform.QuantStrategy;
+import utils.PricePosChecker;
+import utils.TranDaysChecker;
+import utils.TranReportor;
+import utils.XStockSelectManager;
+import utils.ZCZXChecker;
+import utils.PricePosChecker.ResultDropParam;
+
+public abstract class QS1711Base extends QuantStrategy {
+	
+	public QS1711Base(int iMaxSelectCount, int iMaxHoldCount)
+	{
+		m_iMaxSelectCount = iMaxSelectCount;
+		m_iMaxHoldCount = iMaxHoldCount;
+	}
+	
+	public void trySelect(QuantContext ctx, String stockID,  double priority)
+	{
+		m_XStockSelectManager.addSelect(stockID, priority);
+	}
+	
+	public void tryBuy(QuantContext ctx, String stockID)
+	{
+		DAStock cDAStock = ctx.pool().get(stockID);
+		double fNowPrice = cDAStock.price();
+		
+		List<HoldStock> ctnHoldStockList = new ArrayList<HoldStock>();
+		ctx.ap().getHoldStockList(ctnHoldStockList);
+		if(ctnHoldStockList.size() < m_iMaxHoldCount)
+		{
+			CObjectContainer<Double> ctnTotalAssets = new CObjectContainer<Double>();
+			ctx.ap().getTotalAssets(ctnTotalAssets);
+			CObjectContainer<Double> ctnMoney = new CObjectContainer<Double>();
+			ctx.ap().getMoney(ctnMoney);
+			double dCreateMoney = (ctnMoney.get() > ctnTotalAssets.get()/m_iMaxHoldCount)?ctnTotalAssets.get()/m_iMaxHoldCount:ctnMoney.get();
+			int iCreateAmount = (int) (dCreateMoney/fNowPrice)/100*100;
+			if(iCreateAmount > 0)
+			{
+				ctx.ap().pushBuyOrder(stockID, iCreateAmount, fNowPrice);
+			}
+		}
+	}
+	
+	public void trySell(QuantContext ctx, String stockID)
+	{
+		DAStock cDAStock = ctx.pool().get(stockID);
+		double fNowPrice = cDAStock.price();
+		
+		List<HoldStock> ctnHoldStockList = new ArrayList<HoldStock>();
+		ctx.ap().getHoldStockList(ctnHoldStockList);
+		for(int i=0; i<ctnHoldStockList.size(); i++)
+		{
+			HoldStock cHoldStock = ctnHoldStockList.get(i);
+			if(cHoldStock.stockID.equals(stockID))
+			{
+				ctx.ap().pushSellOrder(cHoldStock.stockID, cHoldStock.availableAmount, fNowPrice);
+			}
+		}
+	}
+	
+	@Override
+	public void onInit(QuantContext ctx) {
+		m_XStockSelectManager = new XStockSelectManager(ctx.ap());
+		m_TranReportor = new TranReportor("R");
+		this.onStrateInit(ctx);
+	}
+	@Override
+	public void onDayStart(QuantContext ctx) {
+		CLog.output("TEST", "onDayStart %s", ctx.date());
+		m_XStockSelectManager.loadFromFile();
+		super.addCurrentDayInterestMinuteDataIDs(m_XStockSelectManager.validSelectListS1(m_iMaxSelectCount));
+		CLog.output("TEST", "%s", m_XStockSelectManager.dumpSelect());
+		this.onStrateDayStart(ctx);
+	}
+	
+	@Override
+	public void onMinuteData(QuantContext ctx) {
+		
+		// buy check
+		List<String> validSelectList = m_XStockSelectManager.validSelectListS1(m_iMaxSelectCount);
+		for(int iStock=0; iStock<validSelectList.size(); iStock++)
+		{
+			String selectStockID = validSelectList.get(iStock);
+			DAStock cDAStock = ctx.pool().get(selectStockID);
+			this.onStrateBuyCheck(ctx, cDAStock);
+		}
+		
+		// sell check
+		List<HoldStock> ctnHoldStockList = new ArrayList<HoldStock>();
+		ctx.ap().getHoldStockList(ctnHoldStockList);
+		for(int i=0; i<ctnHoldStockList.size(); i++)
+		{
+			HoldStock cHoldStock = ctnHoldStockList.get(i);
+			DAStock cDAStock = ctx.pool().get(cHoldStock.stockID);
+			this.onStrateSellCheck(ctx, cDAStock, cHoldStock);
+		}
+	}
+	
+	@Override
+	public void onDayFinish(QuantContext ctx) {
+
+		m_XStockSelectManager.clearSelect();
+
+		for(int iStock=0; iStock<ctx.pool().size(); iStock++)
+		{
+			DAStock cDAStock = ctx.pool().get(iStock);
+			this.onStrateDayFinish(ctx, cDAStock);
+		}
+
+		m_XStockSelectManager.saveToFile();
+		
+		// report
+		CObjectContainer<Double> ctnTotalAssets = new CObjectContainer<Double>();
+		ctx.ap().getTotalAssets(ctnTotalAssets);
+		double dSH = ctx.pool().get("999999").price();
+		m_TranReportor.InfoCollector(ctx.date(), ctnTotalAssets.get(), dSH);
+		CLog.output("TEST", "dump account&select\n %s\n    -%s", ctx.ap().dump(), m_XStockSelectManager.dumpSelect());
+	}
+	
+	/*
+	 * 策略初始化
+	 * 程序启动时只调用一次
+	 */
+	abstract void onStrateInit(QuantContext ctx);
+	/*
+	 * 策略每天启动
+	 * 每个交易日开始交易前调用一次
+	 */
+	abstract void onStrateDayStart(QuantContext ctx);
+	/*
+	 * 策略买入检查
+	 * 交易期间每分钟对每个选入股票进行回调
+	 * 用户调用tryBuy进行买入
+	 */
+	abstract void onStrateBuyCheck(QuantContext ctx, DAStock cDAStock);
+	/*
+	 * 策略卖出检查
+	 * 交易期间每分钟对每个持有股票进行回调
+	 * 用户调用trySell进行卖出
+	 */
+	abstract void onStrateSellCheck(QuantContext ctx, DAStock cDAStock, HoldStock cHoldStock);
+	/*
+	 * 策略选股
+	 * 每天交易结束更新数据后对市场所有股票进行回调
+	 * 用户调用trySelect进行选股
+	 */
+	abstract void onStrateDayFinish(QuantContext ctx, DAStock cDAStock);
+
+	private int m_iMaxSelectCount;
+	private int m_iMaxHoldCount;
+	private XStockSelectManager m_XStockSelectManager;
+	private TranReportor m_TranReportor;
+}
