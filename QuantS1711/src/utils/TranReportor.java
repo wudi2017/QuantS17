1feb5e96e1f78fdcb29219e132a2420c875a5879
wudi <wudi19870903@163.com>
2017-12-12 18:03:57 +0800

    x

--- QuantS1711/src/utils/TranReportor.java
+++ QuantS1711/src/utils/TranReportor.java
@@ -24,7 +24,7 @@ public class TranReportor {
 	
 	public TranReportor(String name)
 	{
-		m_imgFileName = CSystem.getRunSessionRoot() + "\\report_" + name + ".jpg";
+		m_name = name;
 		m_cDailyReportTreeMap = new TreeMap<String, DailyReport>((o1, o2) -> {
 			String str1 = (String)o1;
 			String str2 = (String)o2;
@@ -64,32 +64,176 @@ public class TranReportor {
 	
 	public void generateReport()
 	{
+		generate_abs();
+		generate_ref_month();
+		generate_ref_year();
+	}
+	
+	private void generate_abs()
+	{
 		List<CurvePoint> cCurvePointList_TotalAssets = new ArrayList<CurvePoint>();
 		List<CurvePoint> cCurvePointList_SHComposite = new ArrayList<CurvePoint>();
+		List<Double> spanList = new ArrayList<Double>();
 		
+		// 按照key值排序遍历，生成曲线
+		int iSize = m_cDailyReportTreeMap.size();
 		int index=0;
+		String lastDate = "";
 		for (Map.Entry<String, DailyReport> entry : m_cDailyReportTreeMap.entrySet()) { 
 			 String date = entry.getKey();
 			 DailyReport cDailyReport = entry.getValue();
 			 
+			 if(lastDate.equals("")) lastDate = date;
+			 
 			 cCurvePointList_TotalAssets.add(new CurvePoint(index, cDailyReport.dTotalAssets));
 			 cCurvePointList_SHComposite.add(new CurvePoint(index, cDailyReport.dSHComposite));
-				
+			
+			 if(iSize <= 30)
+			 {
+				spanList.add(index/(double)iSize);
+			 }
+			 else if(iSize > 30 && iSize <= 600)
+			 {
+				 if(!lastDate.substring(5, 7).equals(date.substring(5, 7)))
+				 {
+					 spanList.add(index/(double)iSize);
+				 }
+			 }
+			 else if(iSize > 600)
+			 {
+				 if(!lastDate.substring(0, 4).equals(date.substring(0, 4)))
+				 {
+					 spanList.add(index/(double)iSize);
+				 }
+			 }
+			 
 			 index++;
+			 lastDate = date;
 		}
 		
-		CImageCurve cCImageCurve = new CImageCurve(2560,1920,m_imgFileName);
-		cCImageCurve.setColor(Color.BLUE);
-		cCImageCurve.writeLogicCurveSameRatio(cCurvePointList_SHComposite);
-		cCImageCurve.setColor(Color.GREEN);
-		cCImageCurve.writeLogicCurveSameRatio(cCurvePointList_TotalAssets);
-		cCImageCurve.writeMultiLogicCurveSameRatio();
-		cCImageCurve.setColor(Color.BLACK);
-		cCImageCurve.writeAxis();
-		cCImageCurve.GenerateImage();
-		
+		// 把曲线表示成同比图
+		{
+			String filename = CSystem.getRunSessionRoot() + "\\report_abs_" + m_name + ".jpg";
+			CImageCurve cCImageCurve_abs = new CImageCurve(2560,1920,filename);
+			cCImageCurve_abs.setColor(Color.BLUE);
+			cCImageCurve_abs.writeLogicCurveSameRatio(cCurvePointList_SHComposite);
+			cCImageCurve_abs.setColor(Color.GREEN);
+			cCImageCurve_abs.writeLogicCurveSameRatio(cCurvePointList_TotalAssets);
+			cCImageCurve_abs.writeMultiLogicCurveSameRatio();
+			// 绘制坐标和刻度
+			cCImageCurve_abs.setColor(Color.BLACK);
+			cCImageCurve_abs.writeAxis();
+			for(int i=0; i<spanList.size();i++)
+			{
+				double x=spanList.get(i);
+				cCImageCurve_abs.writeUnitLine(x, -0.01, x, 0.01);
+			}
+			cCImageCurve_abs.GenerateImage();
+		}
+	}
+	
+	private void generate_ref_month()
+	{
+		// 月盈亏相对图，参考大盘
+		List<CurvePoint> cCurvePointList_RefRadio = new ArrayList<CurvePoint>();
+		double lastRefVal = 0.0;
+		double lastVal = 0.0;
+		int index=0;
+		String lastDate = "";
+		for (Map.Entry<String, DailyReport> entry : m_cDailyReportTreeMap.entrySet()) { 
+			 String date = entry.getKey();
+			 DailyReport cDailyReport = entry.getValue();
+			 
+			 if(lastDate.equals("")) 
+			 {
+				 lastDate = date;
+				 lastRefVal = cDailyReport.dSHComposite;
+				 lastVal = cDailyReport.dTotalAssets;
+				 cCurvePointList_RefRadio.add(new CurvePoint(0, 0));
+			 }
+			 
+			 if(!lastDate.substring(5, 7).equals(date.substring(5, 7)))
+			 {
+				 // 隔月第一天
+				 double curRefVal = cDailyReport.dSHComposite;
+				 double curVal = cDailyReport.dTotalAssets;
+				 
+				 // 添加参考盈亏比
+				 double refRadio = (curRefVal - lastRefVal)/lastRefVal;
+				 double radio = (curVal - lastVal)/lastVal;
+				 cCurvePointList_RefRadio.add(new CurvePoint(index, radio-refRadio));
+				 
+				 // 保存上次
+				 lastRefVal = cDailyReport.dSHComposite;
+				 lastVal = cDailyReport.dTotalAssets;
+			 }
+			 
+			 index++;
+			 lastDate = date;
+		}
+		// 月参考盈亏图保存
+		{
+			String filename = CSystem.getRunSessionRoot() + "\\report_ref_month_" + m_name + ".jpg";
+			CImageCurve cCImageCurve_abs = new CImageCurve(2560,1920,filename);
+			cCImageCurve_abs.writeLogicCurve(cCurvePointList_RefRadio);
+			// 绘制坐标和刻度
+			cCImageCurve_abs.setColor(Color.BLACK);
+			cCImageCurve_abs.writeAxis();
+			cCImageCurve_abs.GenerateImage();
+		}
+	}
+	
+	private void generate_ref_year()
+	{
+		// 年盈亏相对图，参考大盘
+		List<CurvePoint> cCurvePointList_RefRadio = new ArrayList<CurvePoint>();
+		double lastRefVal = 0.0;
+		double lastVal = 0.0;
+		int index=0;
+		String lastDate = "";
+		for (Map.Entry<String, DailyReport> entry : m_cDailyReportTreeMap.entrySet()) { 
+			 String date = entry.getKey();
+			 DailyReport cDailyReport = entry.getValue();
+			 
+			 if(lastDate.equals("")) 
+			 {
+				 lastDate = date;
+				 lastRefVal = cDailyReport.dSHComposite;
+				 lastVal = cDailyReport.dTotalAssets;
+				 cCurvePointList_RefRadio.add(new CurvePoint(0, 0));
+			 }
+			 
+			 if(!lastDate.substring(0, 4).equals(date.substring(0, 4)))
+			 {
+				 // 隔年第一天
+				 double curRefVal = cDailyReport.dSHComposite;
+				 double curVal = cDailyReport.dTotalAssets;
+				 
+				 // 添加参考盈亏比
+				 double refRadio = (curRefVal - lastRefVal)/lastRefVal;
+				 double radio = (curVal - lastVal)/lastVal;
+				 cCurvePointList_RefRadio.add(new CurvePoint(index, radio-refRadio));
+				 
+				 // 保存上次
+				 lastRefVal = cDailyReport.dSHComposite;
+				 lastVal = cDailyReport.dTotalAssets;
+			 }
+			 
+			 index++;
+			 lastDate = date;
+		}
+		// 年参考盈亏图保存
+		{
+			String filename = CSystem.getRunSessionRoot() + "\\report_ref_year_" + m_name + ".jpg";
+			CImageCurve cCImageCurve_abs = new CImageCurve(2560,1920,filename);
+			cCImageCurve_abs.writeLogicCurve(cCurvePointList_RefRadio);
+			// 绘制坐标和刻度
+			cCImageCurve_abs.setColor(Color.BLACK);
+			cCImageCurve_abs.writeAxis();
+			cCImageCurve_abs.GenerateImage();
+		}
 	}
 	
 	private Map<String, DailyReport> m_cDailyReportTreeMap;
-	private String m_imgFileName;
+	private String m_name;
 }
