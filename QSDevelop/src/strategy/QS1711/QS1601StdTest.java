a28833690029e3205c5eba953aba5005e7a6b803
wudi <wudi19870903@163.com>
2018-03-15 12:00:22 +0800

    add start stop IF for AccOpe

--- QSDevelop/src/strategy/QS1711/QS1601StdTest.java
+++ QSDevelop/src/strategy/QS1711/QS1601StdTest.java
@@ -20,6 +20,17 @@ public class QS1601StdTest {
 	
 	public static class MockMarketOpe extends IMarketOpe
 	{
+		
+		@Override
+		public int start() {
+			return 0;
+		}
+
+		@Override
+		public int stop() {
+			return 0;
+		}
+		
 		@Override
 		public int postTradeRequest(TRANACT tranact, String id, int amount, double price) {
 			if(tranact == TRANACT.BUY)
