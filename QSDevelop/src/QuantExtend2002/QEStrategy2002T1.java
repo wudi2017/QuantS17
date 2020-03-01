411127fcc67ebbb47f902d0d09d21c3e1a336ab4
Your Name <you@example.com>
2020-03-01 18:04:07 +0800

    x

--- QSDevelop/src/QuantExtend2002/QEStrategy2002T1.java
+++ QSDevelop/src/QuantExtend2002/QEStrategy2002T1.java
@@ -1,5 +1,6 @@
 package QuantExtend2002;
 
+import QuantExtend1711.utils.ZCZXChecker;
 import pers.di.account.AccountController;
 import pers.di.account.IAccount;
 import pers.di.common.CLog;
@@ -24,36 +25,50 @@ public class QEStrategy2002T1 extends QEBase2002 {
 
 	@Override
 	void onStrateMinute(QuantContext ctx, DAStock cDAStock) {
-		// TODO Auto-generated method stub
-		if (ctx.time().equals("13:00:00")) {
-			if (cDAStock.timePrices().size() != 0 ) {
-				CLog.output(TAG, "onStrateMinute %s %s %s %.02f (timePrices newest Price)", ctx.date(), ctx.time(), cDAStock.ID(), cDAStock.price());
-			} else {
-				CLog.output(TAG, "onStrateMinute %s %s %s %.02f (last day close price)", ctx.date(), ctx.time(), cDAStock.ID(), cDAStock.price());
-			}
-			
-			if (ctx.date().equals("2020-02-04")) {
-				this.transactionController().buySignalEmit(ctx, cDAStock.ID());
-				CLog.output(TAG, "onStrateMinute %s %s transactionController().buySignalEmit", ctx.date(), ctx.time());
-			}
-			
-		}
+		this.transactionController().buySignalEmit(ctx, cDAStock.ID());
+//		if (ctx.time().equals("13:00:00")) {
+//			if (cDAStock.timePrices().size() != 0 ) {
+//				CLog.output(TAG, "onStrateMinute %s %s %s %.02f (timePrices newest Price)", ctx.date(), ctx.time(), cDAStock.ID(), cDAStock.price());
+//			} else {
+//				CLog.output(TAG, "onStrateMinute %s %s %s %.02f (last day close price)", ctx.date(), ctx.time(), cDAStock.ID(), cDAStock.price());
+//			}
+//			
+//			if (ctx.date().equals("2020-02-04")) {
+//				this.transactionController().buySignalEmit(ctx, cDAStock.ID());
+//				CLog.output(TAG, "onStrateMinute %s %s transactionController().buySignalEmit", ctx.date(), ctx.time());
+//			}
+//			
+//		}
 	}
 
 	@Override
 	void onStrateDayFinish(QuantContext ctx) {
-		// TODO Auto-generated method stub
 		
-		if(ctx.date().equals("2020-02-03")) {
-			this.selector().setMaxCount(3);
-			for (int i = 0; i < ctx.pool().size(); i++) {
-				DAStock cStock = ctx.pool().get(i);
-				if(cStock.ID().equals("600000")) {
-					this.selector().add(cStock.ID(), 0);
-					CLog.output(TAG, "onStrateDayFinish %s %s selector: %s", ctx.date(), ctx.time(), cStock.ID());
+		for (int i = 0; i < ctx.pool().size(); i++) {
+			DAStock cStock = ctx.pool().get(i);
+			//CLog.output(TAG, "onStrateDayFinish %s check:%s", ctx.date(), cStock.ID());
+			//if (cStock.ID().compareTo("600033") >= 0 && cStock.ID().compareTo("600033") <= 0) {
+				boolean bZCZX = ZCZXChecker.check(cStock.dayKLines(), cStock.dayKLines().size()-1);
+				if(bZCZX) {
+					CLog.output(TAG, "onStrateDayFinish %s ZCZX selector:%s", ctx.date(), cStock.ID());
 				}
-			}
+//			}
+			//}
+			
+
 		}
+		
+		
+//		if(ctx.date().equals("2020-02-03")) {
+//			this.selector().setMaxCount(3);
+//			for (int i = 0; i < ctx.pool().size(); i++) {
+//				DAStock cStock = ctx.pool().get(i);
+//				if(cStock.ID().equals("600000")) {
+//					this.selector().add(cStock.ID(), 0);
+//					CLog.output(TAG, "onStrateDayFinish %s %s selector: %s", ctx.date(), ctx.time(), cStock.ID());
+//				}
+//			}
+//		}
 		
 	}
 
@@ -67,7 +82,7 @@ public class QEStrategy2002T1 extends QEBase2002 {
 		cAccountController.open("fast_mock001", true);
 		cAccountController.reset(100000);
 		
-		Quant.instance().run("HistoryTest 2020-02-01 2020-02-20", cAccountController, new QEStrategy2002T1());
+		Quant.instance().run("HistoryTest 2019-01-01 2020-02-20", cAccountController, new QEStrategy2002T1());
 		
 		CLog.output(TAG, "%s", cAccountController.account().dump());
 		
---
+++ QSDevelop/src/QuantExtend2002/TestQEBase2002.java
@@ -0,0 +1,101 @@
+package QuantExtend2002;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pers.di.account.AccountController;
+import pers.di.account.common.CommissionOrder;
+import pers.di.common.CLog;
+import pers.di.common.CObjectContainer;
+import pers.di.common.CSystem;
+import pers.di.common.CTest;
+import pers.di.dataengine.DAStock;
+import pers.di.quantplatform.Quant;
+import pers.di.quantplatform.QuantContext;
+
+public class TestQEBase2002 extends QEBase2002 {
+	public static String TAG = "TEST";
+	@Override
+	void onStrateInit(QuantContext ctx) {
+	}
+
+	@Override
+	void onStrateDayStart(QuantContext ctx) {
+	}
+
+	@Override
+	void onStrateMinute(QuantContext ctx, DAStock cDAStock) {
+		if (ctx.time().equals("13:00:00")) {
+			if (cDAStock.timePrices().size() != 0 ) {
+				CLog.output(TAG, "onStrateMinute %s %s %s %.02f (timePrices newest Price)", ctx.date(), ctx.time(), cDAStock.ID(), cDAStock.price());
+			} else {
+				CLog.output(TAG, "onStrateMinute %s %s %s %.02f (last day close price)", ctx.date(), ctx.time(), cDAStock.ID(), cDAStock.price());
+			}
+			
+			if (ctx.date().equals("2020-02-04")) {
+				CTest.EXPECT_DOUBLE_EQ(cDAStock.price(), 10.60, 2);
+				this.transactionController().buySignalEmit(ctx, cDAStock.ID());
+				CLog.output(TAG, "onStrateMinute %s %s transactionController().buySignalEmit", ctx.date(), ctx.time());
+			}
+			
+			if (ctx.date().equals("2020-02-17")) {
+				CTest.EXPECT_DOUBLE_EQ(cDAStock.price(), 11.04, 2);
+				this.transactionController().sellSignalEmit(ctx, cDAStock.ID());
+				CLog.output(TAG, "onStrateMinute %s %s transactionController().sellSignalEmit", ctx.date(), ctx.time());
+			}
+			
+		}
+	}
+
+	@Override
+	void onStrateDayFinish(QuantContext ctx) {
+		if(ctx.date().equals("2020-02-03")) {
+			this.selector().setMaxCount(3);
+			for (int i = 0; i < ctx.pool().size(); i++) {
+				DAStock cStock = ctx.pool().get(i);
+				if(cStock.ID().equals("600000")) {
+					this.selector().add(cStock.ID(), 0);
+					CLog.output(TAG, "onStrateDayFinish %s %s selector: %s", ctx.date(), ctx.time(), cStock.ID());
+				}
+			}
+		}
+		if (ctx.date().equals("2020-02-04")) {
+			List<CommissionOrder> commissionList = new ArrayList<CommissionOrder>();
+			ctx.accountProxy().getCommissionOrderList(commissionList);
+			CTest.EXPECT_LONG_EQ(commissionList.size(), 1);
+			CTest.EXPECT_LONG_EQ(commissionList.get(0).amount, 1800);
+			CTest.EXPECT_DOUBLE_EQ(commissionList.get(0).price, 10.60);
+		}
+		
+	}
+
+	@CTest.test
+	public static void test_QEBase2002() {
+		CLog.output(TAG, "test_QEBase2002 main begin");
+		
+		AccountController cAccountController = new AccountController(CSystem.getRWRoot() + "\\account");
+		cAccountController.open("TestQEBase2002_account", true);
+		cAccountController.reset(100000);
+		
+		Quant.instance().run("HistoryTest 2020-02-01 2020-02-20", cAccountController, new TestQEBase2002());
+		
+		CLog.output(TAG, "%s", cAccountController.account().dump());
+		
+		CObjectContainer<Double> ctnTotalAssets = new CObjectContainer<Double>();
+		cAccountController.account().getTotalAssets(ctnTotalAssets);
+		CTest.EXPECT_DOUBLE_EQ(ctnTotalAssets.get(), 100761.349, 2); 
+		// costBuy: 5+(1800*10.6)*0.00002=5.3816=5.382 costSell: 5+0.39744+19.872=25.26944=25.269
+		// profit: (11.04-10.6)*1800=792
+		// 792-5.382-25.269=761.349
+		cAccountController.close();
+		
+		CLog.output(TAG, "test_QEBase2002 main end");
+	}
+	
+	public static void main(String[] args) throws Exception {
+		CSystem.start();
+		CTest.ADD_TEST(TestQEBase2002.class);
+		CTest.RUN_ALL_TESTS("TestQEBase2002.");
+		CSystem.stop();
+	}
+}
