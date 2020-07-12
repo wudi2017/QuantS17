a8459baf21cf4bedf2e74ceb4d56be9c2e8b9e1d
Your Name <you@example.com>
2020-07-12 21:03:41 +0800

    x

---
+++ QSDevelop/src/QuantExtend2002_test/TestSingle.java
@@ -0,0 +1,60 @@
+package QuantExtend2002_test;
+
+import QuantExtend2002.framework.QEBase2002;
+import QuantExtend2002.utils.ExtEigenMorningCross;
+import pers.di.account.AccountController;
+import pers.di.common.CListObserver;
+import pers.di.common.CLog;
+import pers.di.common.CSystem;
+import pers.di.common.CTest;
+import pers.di.dataengine.DAStock;
+import pers.di.localstock.LocalStock;
+import pers.di.localstock.common.KLine;
+import pers.di.quantplatform.Quant;
+import pers.di.quantplatform.QuantContext;
+
+public class TestSingle {
+	public static String TAG = "TEST";
+	
+	public static class RunQEStrategyTESTSingle extends QEBase2002 {
+
+		@Override
+		public void onStrateInit(QuantContext ctx) {
+		}
+		
+		@Override
+		public void onStrateDayStart(QuantContext ctx) {
+		}
+		
+		@Override
+		public void onStrateMinute(QuantContext ctx, DAStock cDAStock) {
+		}
+
+		@Override
+		public void onStrateDayFinish(QuantContext ctx) {
+			// transfer all stock
+			DAStock cStock = ctx.pool().get("000543");
+			
+			boolean bCk = ExtEigenMorningCross.check(cStock.dayKLines(), cStock.dayKLines().size()-1);
+			CLog.output(TAG, "%s price:%.3f %b", cStock.date(), cStock.price(), bCk);
+		
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		CSystem.start();
+		CLog.output(TAG, "RunQEStrategy2002T1 main begin");
+		
+		AccountController cAccountController = new AccountController(CSystem.getRWRoot() + "\\account");
+		cAccountController.open("fast_mock001", true);
+		cAccountController.reset(100000);
+		// "HistoryTest 2019-01-01 2020-02-20" "Realtime"
+		Quant.instance().run("HistoryTest 2020-01-01 2020-03-31", cAccountController, new RunQEStrategyTESTSingle()); 
+		CLog.output(TAG, "%s", cAccountController.account().dump());
+		cAccountController.close();
+		
+		CSystem.stop();
+		CLog.output(TAG, "RunQEStrategy2002T1 main end");
+	}
+}
